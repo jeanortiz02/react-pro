8f7e4705a273876cbd5f0138470f16b41b61ca80
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2025-01-20 10:54:44 -0400

    todo completo

--- 02-react-app/src/02-component-parrents/pages/ProductCard.tsx
+++ 02-react-app/src/02-component-parrents/pages/ProductCard.tsx
@@ -1,18 +1,66 @@
 
+import { useProduct } from '../hooks/useProduct';
 import style from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 
-export const ProductCard = () => {
 
-    // console.log(style);
+interface Props {
+  product: Product;
+}
+
+interface Product {
+  id: string;
+  title: string;
+  image?: string;
+}
+
+
+
+
+export const ProductImage = ({ image  = ''})=> {
+  return (
+    <img className={ style.productImg } src={ image ? image : noImage} alt="Product Image" />
+  )
+}
+
+export const ProductTitle = ({ title } : { title: string}) => {
+  return (
+    <span className={style.productDescription}>{title}</span>
+  )
+}
+
+interface ProductButtonProps {
+  counter: number;
+  increaseBy: (value: number) => void;
+}
+
+export const ProductButton = ( { counter, increaseBy} : ProductButtonProps ) => {
+  return (
+    <div className={ style.buttonsContainer }>
+
+      <button className={ style.buttonMinus } onClick={ ()=> increaseBy( -1 ) }>-</button>
+
+      <div className={ style.countLabel }>{counter}</div>
+      
+      <button className={ style.buttonAdd } onClick={ ()=> increaseBy( 1 ) }>+</button>
+
+    </div>
+  )
+}
+
+
+export const ProductCard = ({ product } : Props) => {
+
+    const { counter, increaseBy } = useProduct();
 
   return (
     <div className={style.productCard}>
-        {/* <img className={ style.productImg } src="./coffee-mug.png" alt="Coffe Mug" /> */}
-        <img className={ style.productImg } src={ noImage } alt="Coffe Mug" />
 
-        <span className={style.productDescription}>Coffe Mud</span>
+      <ProductImage image={product.image} />
 
+      <ProductTitle title={product.title}/>
+
+      <ProductButton counter={counter} increaseBy={increaseBy} />
 
 
     </div>
--- 02-react-app/src/02-component-parrents/pages/ShoppingPage.tsx
+++ 02-react-app/src/02-component-parrents/pages/ShoppingPage.tsx
@@ -1,12 +1,27 @@
 import { ProductCard } from "./ProductCard"
 
 
+const product = {
+  id: '1',
+  title: "Coffe Mug",
+  image: "./coffee-mug.png"
+}
+
 export const ShoppingPage = () => {
   return (
     <div>
         <h1>Shopping Store</h1>
         <hr />
-        <ProductCard/>
+
+        <div 
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+          }}
+        >
+          <ProductCard product={product }/>
+        </div>
     </div>
   )
 }
