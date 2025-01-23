fb04e71b77831d69a28567b01062638a59838b21
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2025-01-23 14:04:34 -0400

    Control Props alt

--- 02-react-app/src/02-component-parrents/hooks/useShoppingCart.ts
+++ 02-react-app/src/02-component-parrents/hooks/useShoppingCart.ts
@@ -9,28 +9,17 @@ export const useShoppingCart = () => {
       const onProductCountChange = ({count, product} : {count: number, product: Product}) => {
         
         setShoppingCart(oldShoppingCart => {
+
+          console.log({ count });
+          if (count === 0 ){
+              const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+              return rest;
+          }
     
-            const productInCart : ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-    
-            if( Math.max(productInCart.count + count, 0) > 0) {
-    
-              productInCart.count += count;
-    
-              return {
-                ...oldShoppingCart,
-                [product.id] : productInCart
-              }
-    
-            }
-            
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest
-          // }
-    
-          // return {
-          //   ...oldShoppingCart,
-          //   [product.id] : {...product, count}
-          // }
+          return {
+            ...oldShoppingCart,
+            [product.id] : {...product, count}
+          }
         })
     }
 
