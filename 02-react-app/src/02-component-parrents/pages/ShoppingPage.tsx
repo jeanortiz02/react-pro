cddcb261398c6528740a08d6bac2359e15cc62ec
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2025-01-24 16:04:06 -0400

    04 videos

--- 02-react-app/src/02-component-parrents/pages/ShoppingPage.tsx
+++ 02-react-app/src/02-component-parrents/pages/ShoppingPage.tsx
@@ -1,5 +1,4 @@
 import { ProductButton, ProductCard, ProductImage, ProductTitle } from "../components"
-import '../styles/custom-style.css';
 import { products } from "../data/product";
 
 const product = products[0];
@@ -18,7 +17,6 @@ export const ShoppingPage = () => {
           <ProductCard 
             key={product.id} 
             product={product}
-            className="bg-dark text-white"
             initialValues={{
               count: 6,
               maxCount: 10,
@@ -28,18 +26,9 @@ export const ShoppingPage = () => {
             {
               ({ reset, increaseBy, count, isMaxCountReached})=> (
                 <>
-                  <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgb(0,0,0,0.20)'}}/>
-                  <ProductTitle className="text-bold"/>
-                  <ProductButton className="custom-bottons"/>
-                  <button onClick={reset}>Reset</button>
-
-                  <button onClick={() =>increaseBy(-2)}>-2</button>
-
-                  {
-                    (!isMaxCountReached && <button onClick={() =>increaseBy(2)}>+2</button>)
-                  }
-                  
-                  <span>{count}</span>
+                  <ProductImage/>
+                  <ProductTitle />
+                  <ProductButton/>
                 </>
               )
             }
---
+++ jc-product-card/README.md
@@ -0,0 +1,7 @@
+# JC-product-card
+
+Este es un paquete de pruebas de despliegue de NPM
+
+### Jean Carlos 
+
+## Ejemplo
